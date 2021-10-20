80b8f6ef70e00919f368e07ac0d2326a53153e21
Federico López Gómez <fico89@gmail.com>
2021-10-20 08:08:12 +0100

    Adding Cookie Consent banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { HashRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Link, Route, Switch } from "react-router-dom";
 import { ContactPage } from "./pages/ContactPage";
 import { DetailsPage } from "./pages/DetailsPage";
 import { HomePage } from "./pages/HomePage";
@@ -9,6 +9,8 @@ import { SearchPage } from "./pages/SearchPage";
 import { SubmitPage } from "./pages/SubmitPage";
 import { TermsOfUsePage } from "./pages/TermsOfUsePage";
 import { routes } from "./Routes";
+import CookieConsent from "react-cookie-consent";
+import { Button } from "react-bootstrap";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -44,6 +46,19 @@ function App() {
           ))}
         </Switch>
       </QueryClientProvider>
+      <CookieConsent
+        buttonText="I agree, dismiss this banner"
+        containerClasses="p-lg-5 p-3 bg-white text-dark border-top border-primary"
+        style={{ borderWidth: "5px !important" }}
+        buttonClasses="btn btn-primary"
+        disableButtonStyles={true}
+      >
+        This website requires cookies, and the limited processing of your
+        personal data in order to function. By using the site you are agreeing
+        to this as outlined in our{" "}
+        <Link to="/about/privacy-policy">Privacy Notice</Link> and{" "}
+        <Link to="/about/terms-of-use">Terms of Use</Link>.
+      </CookieConsent>
     </HashRouter>
   );
 }
