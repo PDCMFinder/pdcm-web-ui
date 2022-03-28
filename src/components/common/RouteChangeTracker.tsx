993b2b00148f3d0c3218a53773c34040d04dcc06
Federico López Gómez <federico@ebi.ac.uk>
2022-03-28 11:02:11 +0100

    Update RouteChangeTracker to use GA4

--- src/components/common/RouteChangeTracker.tsx
+++ src/components/common/RouteChangeTracker.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
-import ReactGA from "react-ga";
+import ReactGA from "react-ga4";
 
 const RouteChangeTracker = ({ history }: { history: any }) => {
   history.listen((location: any, action: any) => {
     ReactGA.set({ page: location.pathname });
-    ReactGA.pageview(location.pathname);
+    ReactGA.send(location.pathname);
   });
 
   return <div></div>;
