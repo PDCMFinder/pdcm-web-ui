06fc70f3c7ccb9be03960d35ed182680d49fd21a
Federico López Gómez <federico@ebi.ac.uk>
2022-03-31 14:52:04 +0100

    Fix Footer links and Contact nav item

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -8,15 +8,15 @@ export const ContactPage: FunctionComponent = () => {
     <GeneralTemplate>
       <Container className="mt-5">
         <h1>Contact</h1>
-        <h4>PDX Finder Feedback</h4>
+        <h4>PDCM Finder Feedback</h4>
         <p>
-          PDX Finder is continuously developed in response to community&rsquo;s
+          PDCM Finder is continuously developed in response to community&rsquo;s
           needs. We need your feedback to improve and refine the PDX Finder.
         </p>
         <p>
           Please email us&nbsp;to ask questions, report issues or request
           features:&nbsp;
-          <a href="mailto:helpdesk@pdxfinder.org?subject=PDX%20Finder%20general%20inquiry">
+          <a href="mailto:helpdesk@pdxfinder.org?subject=PDCM%20Finder%20general%20inquiry">
             helpdesk&nbsp;
           </a>
         </p>
