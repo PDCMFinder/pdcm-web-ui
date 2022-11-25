cdc8c9d5855c8cacd87b38b7bddb99da106d42e1
Federico López Gómez <federico@ebi.ac.uk>
2022-11-25 15:48:43 +0000

    Add recaptcha logic

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,10 +1,55 @@
-import React, { FunctionComponent } from "react";
+import React, {
+  FunctionComponent,
+  useCallback,
+  useEffect,
+  useState,
+} from "react";
 import { Col, Row, Container } from "react-bootstrap";
 import { GeneralTemplate } from "../templates/GeneralTemplate";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { useGoogleReCaptcha } from "react-google-recaptcha-v3";
+import { createTicket } from "../apis/Contact.api";
 
 export const ContactPage: FunctionComponent = () => {
+  const { executeRecaptcha } = useGoogleReCaptcha();
+
+  const [userName, setName] = useState("");
+  const [userEmail, setEmail] = useState("");
+  const [feedback, setFeedback] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+  const [validated, setValidated] = useState(false);
+  const [valid, setValid] = useState(false);
+
+  const handleReCaptchaVerify = useCallback(async () => {
+    if (!executeRecaptcha) {
+      console.log("Execute recaptcha not yet available");
+      return;
+    }
+
+    const token = await executeRecaptcha("yourAction");
+    if (valid) {
+      const response = await createTicket(userName, userEmail, feedback, token);
+      console.log(response);
+    }
+  }, [executeRecaptcha, userName, userEmail, feedback, valid]);
+
+  useEffect(() => {
+    handleReCaptchaVerify();
+  }, [handleReCaptchaVerify]);
+
+  const handleSubmit = (event: any) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    } else {
+      setValid(true);
+    }
+
+    setValidated(true);
+  };
+
   return (
     // file name in /static/content,  without `.md` file extension
     <GeneralTemplate>
@@ -17,13 +62,14 @@ export const ContactPage: FunctionComponent = () => {
         </p>
         <Col md={6} lg={4}>
           <Row>
-            <Form>
-              <Form.Group className="mb-3" controlId="formName">
+            <Form noValidate validated={validated} onSubmit={handleSubmit}>
+              <Form.Group
+                className="mb-3"
+                controlId="formName"
+                onChange={(e) => setName((e.target as HTMLInputElement).value)}
+              >
                 <Form.Label>Your Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Enter your complete name"
-                />
+                <Form.Control type="text" placeholder="Enter your full name" />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formEmail">
@@ -43,10 +89,10 @@ export const ContactPage: FunctionComponent = () => {
                 />
               </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formFile">
+              {/* <Form.Group className="mb-3" controlId="formFile">
                 <Form.Label>Attachment</Form.Label>
                 <Form.Control type="file" />
-              </Form.Group>
+              </Form.Group> */}
 
               <Button variant="primary" type="submit">
                 Send feedback
