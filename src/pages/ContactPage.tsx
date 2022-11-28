2050ab0178bb6c035275061850a401368c9f9659
Federico López Gómez <federico@ebi.ac.uk>
2022-11-28 18:04:29 +0000

    Adding re-captcha support

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -28,11 +28,20 @@ export const ContactPage: FunctionComponent = () => {
     }
 
     const token = await executeRecaptcha("yourAction");
-    if (valid) {
+    if (valid && submitted) {
       const response = await createTicket(userName, userEmail, feedback, token);
       console.log(response);
+
+      if (!response.error) {
+        setSubmitted(false);
+        setValid(false);
+        setValidated(false);
+        setName("");
+        setEmail("");
+        setFeedback("");
+      }
     }
-  }, [executeRecaptcha, userName, userEmail, feedback, valid]);
+  }, [executeRecaptcha, userName, userEmail, feedback, valid, submitted]);
 
   useEffect(() => {
     handleReCaptchaVerify();
@@ -40,11 +49,12 @@ export const ContactPage: FunctionComponent = () => {
 
   const handleSubmit = (event: any) => {
     const form = event.currentTarget;
+    event.preventDefault();
     if (form.checkValidity() === false) {
-      event.preventDefault();
       event.stopPropagation();
     } else {
       setValid(true);
+      setSubmitted(true);
     }
 
     setValidated(true);
@@ -69,23 +79,44 @@ export const ContactPage: FunctionComponent = () => {
                 onChange={(e) => setName((e.target as HTMLInputElement).value)}
               >
                 <Form.Label>Your Name</Form.Label>
-                <Form.Control type="text" placeholder="Enter your full name" />
+                <Form.Control
+                  type="text"
+                  placeholder="Enter your full name"
+                  required
+                  value={userName}
+                />
               </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formEmail">
+              <Form.Group
+                className="mb-3"
+                controlId="formEmail"
+                onChange={(e) => setEmail((e.target as HTMLInputElement).value)}
+              >
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter your email" />
+                <Form.Control
+                  type="email"
+                  placeholder="Enter your email"
+                  required
+                  value={userEmail}
+                />
                 <Form.Text className="text-muted">
                   We'll never share your email with anyone else.
                 </Form.Text>
               </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formMessage">
+              <Form.Group
+                className="mb-3"
+                controlId="formMessage"
+                onChange={(e) =>
+                  setFeedback((e.target as HTMLInputElement).value)
+                }
+              >
                 <Form.Label>Your Feedback *</Form.Label>
                 <Form.Control
                   as="textarea"
                   placeholder="Please write here your feedback"
                   required={true}
+                  value={feedback}
                 />
               </Form.Group>
 
