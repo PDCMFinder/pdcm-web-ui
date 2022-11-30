78d0b7de76bcf4d89019be69c2082f334ef75215
Federico López Gómez <federico@ebi.ac.uk>
2022-11-30 11:40:51 +0000

    Remove recaptcha badge from all the pages and add feddback modal

--- src/templates/GeneralTemplate.tsx
+++ src/templates/GeneralTemplate.tsx
@@ -5,12 +5,23 @@ import { MainNavBar } from "../components/common/MainNavBar";
 import "./GeneralTemplate.scss";
 import { Breadcrumbs } from "../components/common/Breadcrumbs";
 
-export const GeneralTemplate: FunctionComponent = ({ children }) => {
+export const GeneralTemplate: FunctionComponent<{
+  showRecaptcha?: boolean;
+}> = ({ showRecaptcha, children }) => {
   const MainNavBarWithRouter = withRouter(MainNavBar);
   const BreadcrumbsWithRouter = withRouter(Breadcrumbs);
   return (
     <div className="m-auto" style={{ height: "100%", maxWidth: "2000px" }}>
       <MainNavBarWithRouter />
+      {showRecaptcha ? (
+        <style>{`.grecaptcha-badge {
+        bottom: 140px !important;
+        }`}</style>
+      ) : (
+        <style>{`.grecaptcha-badge {
+        display: none !important;
+        }`}</style>
+      )}
 
       {process.env.REACT_APP_UNDER_CONSTRUTION === "true" ? (
         <BreadcrumbsWithRouter />
