3fbd4a46a20f36c0872991fa5b6245c3a08cca3e
Marcelo Rios Almanza <marcelo10rios@hotmail.com>
2023-01-19 14:21:08 +0000

    update floating widget text and link

--- src/templates/GeneralTemplate.tsx
+++ src/templates/GeneralTemplate.tsx
@@ -28,8 +28,8 @@ export const GeneralTemplate: FunctionComponent<{
         <BreadcrumbsWithRouter />
       ) : null}
       <main className="main-content px-5">{children}</main>
-      <FloatingWidget link="https://google.com/">
-        Help us fill our survey
+      <FloatingWidget link="https://docs.google.com/forms/d/e/1FAIpQLSeRJQ7Xu1pMqegYvs4KVdA17bucM6XzW2zzA2yHaroPfSR7Sg/viewform">
+        Help us improve - take our user survey
       </FloatingWidget>
       <Footer className="footer"></Footer>
     </div>
